3f46f9bbb95c0d0839b12527ff79e4c1c7cafb05
Ivo Ursino <ivo.ursino@naranja.com>
2020-01-11 01:37:16 -0300

    Fix input

--- awsprofile.sh
+++ awsprofile.sh
@@ -2,7 +2,7 @@
 
 AWS_PROFILES=$(cat ~/.aws/credentials | grep '\[*\]' | cut -d "[" -f2 | cut -d "]" -f1)
 
-printf "Choose your AWS profile:\n\n"
+printf "\nChoose your AWS profile:\n\n"
 
 COUNT=0
 
@@ -11,11 +11,14 @@ echo "$AWS_PROFILES" | while read -r line ; do
     echo "${COUNT})$line"
 done
 
-read -s -n1 n
+printf "\nSelect profile number and press [ENTER]: "
+
+read n
+
 case $n in
   [0-9]*) MY_AWS_PROFILE=$(echo "$AWS_PROFILES" | awk "NR==$n{print}");;
 esac
 
 export AWS_PROFILE=$MY_AWS_PROFILE
 
-printf "\nThe AWS profile ${AWS_PROFILE} has been set.\n"
+printf "\nThe AWS profile ${AWS_PROFILE} has been set.\n\n"
